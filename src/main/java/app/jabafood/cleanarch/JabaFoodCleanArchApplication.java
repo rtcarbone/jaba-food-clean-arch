2a6f33b7689471d58cc3216d854e9ff3e4529d0d
Carbone <rtcarbone@gmail.com>
2025-02-17 20:41:58 -0300

    Commit inicial

---
+++ src/main/java/app/jabafood/cleanarch/JabaFoodCleanArchApplication.java
@@ -0,0 +1,13 @@
+package app.jabafood.cleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabaFoodCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabaFoodCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/jabafood/cleanarch/JabaFoodCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jabafood.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabaFoodCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
