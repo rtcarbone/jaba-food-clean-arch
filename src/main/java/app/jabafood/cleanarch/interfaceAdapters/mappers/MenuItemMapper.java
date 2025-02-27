756d4a2e109cdbf2fd9346eb9d232aed4750735a
Carbone <rtcarbone@gmail.com>
2025-02-26 22:27:39 -0300

    refact: alteracao de pacotes das implementacoes dos gateways

--- src/main/java/app/jabafood/cleanarch/interfaceAdapters/mappers/MenuItemMapper.java
+++ src/main/java/app/jabafood/cleanarch/interfaceAdapters/mappers/MenuItemMapper.java
@@ -3,19 +3,14 @@ package app.jabafood.cleanarch.interfaceAdapters.mappers;
 import app.jabafood.cleanarch.domain.entities.MenuItem;
 import app.jabafood.cleanarch.interfaceAdapters.dto.MenuItemDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MenuItemMapper {
-    MenuItemMapper INSTANCE = Mappers.getMapper(MenuItemMapper.class);
-
     MenuItemDTO toDTO(MenuItem menuItem);
 
     MenuItem toDomain(MenuItemDTO dto);
 
     List<MenuItemDTO> toDTOList(List<MenuItem> menuItems);
-
-    List<MenuItem> toDomainList(List<MenuItemDTO> dtos);
 }
--- src/main/java/app/jabafood/cleanarch/interfaceAdapters/mappers/UserMapper.java
+++ src/main/java/app/jabafood/cleanarch/interfaceAdapters/mappers/UserMapper.java
@@ -3,19 +3,14 @@ package app.jabafood.cleanarch.interfaceAdapters.mappers;
 import app.jabafood.cleanarch.domain.entities.User;
 import app.jabafood.cleanarch.interfaceAdapters.dto.UserDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
     UserDTO toDTO(User user);
 
     User toDomain(UserDTO dto);
 
     List<UserDTO> toDTOList(List<User> users);
-
-    List<User> toDomainList(List<UserDTO> dtos);
 }
